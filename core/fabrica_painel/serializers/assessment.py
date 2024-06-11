476636416d73ce562006a24b210a955453772d80
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-06-11 16:10:02 -0300

    feat: Add Assessment serializers for detail and write operations

---
+++ core/fabrica_painel/serializers/assessment.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from core.fabrica_painel.models.assessment import Assessment
+
+
+class AssessmentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assessment
+        fields: list[str] = [
+            "id",
+            "registration",
+            "work",
+            "grade",
+            "evaluator",
+            "date_time",
+        ]
+
+class AssessmentWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assessment
+        fields: list[str] = [
+            "registration",
+            "work",
+            "grade",
+            "evaluator",
+            "date_time",
+        ]
+
+
