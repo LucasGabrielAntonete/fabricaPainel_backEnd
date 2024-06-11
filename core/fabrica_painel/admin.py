c29c6452d0220f2bce3786fcc0762c2207d5bb69
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-06-11 16:08:59 -0300

    feat: Register new models in admin site

--- core/fabrica_painel/admin.py
+++ core/fabrica_painel/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from core.fabrica_painel.models.assessment import Assessment
+from core.fabrica_painel.models.assessments import Assessments
+from core.fabrica_painel.models.keyword import Keyword
+from core.fabrica_painel.models.work import Work
+from core.fabrica_painel.models.field import Field
+from core.fabrica_painel.models.edition import Edition
+from core.fabrica_painel.models.team import Team
 
 # Register your models here.
+
+admin.site.register(Assessment)
+admin.site.register(Assessments)
+admin.site.register(Keyword)
+admin.site.register(Work)
+admin.site.register(Field)
+admin.site.register(Edition)
+admin.site.register(Team)
