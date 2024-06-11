abacf9a518623365d7f3c3ec6270ef7fef936569
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-06-11 16:11:24 -0300

    add: serializers

--- core/usuario/models.py
+++ core/usuario/models.py
@@ -3,15 +3,26 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
-
+from core.fabrica_painel.models.edition import Edition
+from core.fabrica_painel.models.field import Field
 
 class Usuario(AbstractUser):
     username = None
     email = models.EmailField(_("e-mail address"), unique=True)
-    cpf = models.CharField(_("CPF"), max_length=11, blank=True, null=True)
-    telefone = models.CharField(_("Phone"), max_length=11, blank=True, null=True)
-    data_nascimento = models.DateField(
-        _("Birth Date"), auto_now=False, auto_now_add=False, blank=True, null=True
+    registration = models.IntegerField(null=True, unique=True)
+    name = models.CharField(max_length=255, null=True)
+    is_advisor = models.BooleanField(default=False, null=True, blank=True)
+    is_evaluator = models.BooleanField(default=False, null=True, blank=True)
+    field = models.ForeignKey(Field, on_delete=models.PROTECT, null=True)
+    class UserType(models.TextChoices):
+        STUDENT = "STUDENT", _("Student")
+        TEACHER = "TEACHER", _("Teacher")
+        ADMIN = "ADMIN", _("Admin")
+
+    user_type = models.CharField(
+        max_length=10,
+        choices=UserType.choices,
+        default=UserType.STUDENT,
     )
 
     USERNAME_FIELD = "email"
@@ -27,3 +38,23 @@ class Usuario(AbstractUser):
         verbose_name = "Usuário"
         verbose_name_plural = "Usuários"
         ordering = ["-date_joined"]
+
+
+class Pre_user(models.Model):
+    class user_type(models.TextChoices):
+        STUDENT = "STUDENT", _("Student")
+        TEACHER = "TEACHER", _("Teacher")
+        ADMIN = "ADMIN", _("Admin")
+    email = models.EmailField(_("e-mail address"), unique=True)
+    registration = models.IntegerField(null=True, unique=True)
+    name = models.CharField(max_length=255, null=True)
+    classroom = models.CharField(max_length=255, null=True)
+    user_type = models.CharField(
+        max_length=10, choices=user_type.choices, default=user_type.STUDENT
+    )
+    edition = models.ForeignKey(Edition, on_delete=models.PROTECT, null=True)
+
+
+
+
+    
