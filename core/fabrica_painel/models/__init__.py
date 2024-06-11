a01436856c1d57fac3c62c1293a4b5d9a7d63554
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-06-11 16:08:43 -0300

    feat: Add Assessment model to manage assessments in the application

---
+++ core/fabrica_painel/models/__init__.py
@@ -0,0 +1,7 @@
+from .field import Field
+from .work import Work
+from .keyword import Keyword
+from .team import Team
+from .edition import Edition
+from .assessments import Assessments
+from .assessment import Assessment
