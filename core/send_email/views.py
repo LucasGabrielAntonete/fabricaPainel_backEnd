736ed10ba7719e6c07c24124241fbcfc95e0637f
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-06-25 20:25:34 -0300

    FeAT: create email api view #5 #6

--- core/send_email/views.py
+++ core/send_email/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class EmailAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        subject = request.POST.get("subject", "Nome do email")
+        message = request.POST.get("message", "Conteúdo do email")
+        recipient_list = ["lucasantonete@gmail.com", "lucasantonete@hotmail.com"]
+        from_email = "lucasantonete.ifc@gmail.com"
+        try:
+            send_mail(
+                subject,
+                message,
+                recipient_list=recipient_list,
+                from_email=from_email,
+            )
+
+        except BadHeaderError:
+            return HttpResponse("Invalid header found.")
+        except ValidationError as e:
+            return HttpResponse(str(e))
+        return HttpResponse({"Email enviado com sucesso"}, status=status.HTTP_200_OK)
