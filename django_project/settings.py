0f982515fe19394e28746ffe346d5d10705cdebf
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-06-25 20:09:56 -0300

    FEAT: get email settings from .env #5 #6

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY") 
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -19,39 +20,38 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'django_extensions',
-    'django_filters',
-    'drf_spectacular',
-    'core.fabrica_painel',
-    'core.usuario',
-    'core.uploader',
-    'core.send_email',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "django_extensions",
+    "django_filters",
+    "drf_spectacular",
+    "core.fabrica_painel",
+    "core.usuario",
+    "core.uploader",
+    "core.send_email",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'django_project.urls'
+ROOT_URLCONF = "django_project.urls"
 
 REST_FRAMEWORK = {
- 
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissions",
     ],
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    }
+}
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Fabrica Painel API",
@@ -69,44 +69,43 @@ SPECTACULAR_SETTINGS = {
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'django_project.wsgi.application'
+WSGI_APPLICATION = "django_project.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-#DATABASES = {
- #   'default': {
-    #   'ENGINE': "django.db.backends.postgresql",
-    #   'NAME': os.getenv("POSTGRES_NAME"),
-    #   'USER': os.getenv("POSTGRES_USER"),
-    #   'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-    #   'HOST': os.getenv("POSTGRES_HOST"), 
-    #   'PORT': os.getenv("POSTGRES_PORT"),       
-   # }
-#}
-
+# DATABASES = {y
+#   'default': {
+#   'ENGINE': "django.db.backends.postgresql",
+#   'NAME': os.getenv("POSTGRES_NAME"),
+#   'USER': os.getenv("POSTGRES_USER"),
+#   'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+#   'HOST': os.getenv("POSTGRES_HOST"),
+#   'PORT': os.getenv("POSTGRES_PORT"),
+# }
+# }
 
 
 # Password validation
@@ -114,16 +113,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -131,9 +130,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -148,25 +147,24 @@ FILE_UPLOAD_PERMISSIONS = 0o640
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
 AUTH_USER_MODEL = "usuario.Usuario"
 
 GRAPH_MODELS = {
-  'all_applications': True,
-  'group_models': True,
+    "all_applications": True,
+    "group_models": True,
 }
 
-EMAIL_BACKEND: str = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST: str = 'smtp.gmail.com'
-EMAIL_PORT: int = 587
-EMAIL_USE_TLS: bool = True
-EMAIL_HOST_USER: str = "email do app gmail do painel"
-EMAIL_HOST_PASSWORD: str = "senha do app gmail do painel"
-
+EMAIL_BACKEND: str = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST: str = os.getenv("EMAIL_HOST")
+EMAIL_PORT: int = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS: bool = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER: str = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD: str = os.getenv("EMAIL_HOST_PASSWORD")
