792ad65ea5689ba7823b58d57bf7fcd85b56a5a5
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-07-05 22:18:14 -0300

    FEAT: only the work advisor can accept the submission #11

--- core/send_email/views/accept_work.py
+++ core/send_email/views/accept_work.py
@@ -1,20 +1,29 @@
 from django.utils import timezone
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from core.fabrica_painel.models import Work
 
 
-@api_view(['GET'])
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def accept_work(request, work_id: int):
     try:
         work: Work = Work.objects.get(id=work_id)
     except Work.DoesNotExist:
         return Response(
-                {"error": "Trabalho não encontrado."},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+            {"error": "Trabalho não encontrado."},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    # Enforces that the one accepting the work submission is work advisor
+    if request.user != work.advisor:
+        return Response(
+            {"error": "Você não tem permissão para aceitar esse trabalho."},
+            status=status.HTTP_403_FORBIDDEN,
+        )
 
     work.initial_submission_work_date = timezone.now()
     work.save()
@@ -22,6 +31,6 @@ def accept_work(request, work_id: int):
     print(work)
 
     return Response(
-                {"message": "Trabalho aceito."},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+        {"message": "Trabalho aceito."},
+        status=status.HTTP_200_OK,
+    )
